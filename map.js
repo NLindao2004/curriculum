cd80265146fb3ab42bc625bc42755d4d00e5a639
NLindao2004 <nlindao@espol.edu.ec>
2024-10-12 21:28:33 -0500

    Avance con la seccion contacto

--- map.js
+++ map.js
@@ -7,3 +7,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([-2.147438, -79.955432]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
+
+
+// Seleccionar el input y el label
+const input = document.getElementById('nombre');
+const label = document.querySelector('.floating-label');
+
+// Añadir el evento focus
+input.addEventListener('focus', function() {
+  label.classList.add('active');
+});
+
+// Añadir el evento blur para cuando se salga del input
+input.addEventListener('blur', function() {
+  if (input.value === '') {
+    label.classList.remove('active');
+  }
+});
